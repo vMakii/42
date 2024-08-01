202ebd906dffc438740c24745472e8d6a7514b88
Mika Vogel <mivogel@student.fr>
2024-08-01 17:17:06 +0200

    ex05

--- c_03/ex05/ft_strlcat.c
+++ c_03/ex05/ft_strlcat.c
@@ -26,30 +26,29 @@ int	ft_strlen(char *str)
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
-	unsigned int	len_dest;
+	unsigned int	ldest;
 	unsigned int	lsrc;
 
-	len_dest = ft_strlen(dest);
+	ldest = ft_strlen(dest);
 	lsrc = ft_strlen(src);
-	if (size <= len_dest)
+	if (size == 0 || size <= ldest)
 		return (size + lsrc);
 	i = 0;
-	while (i + len_dest + 1 < size && src[i])
+	while (i + ldest + 1 < size && src[i])
 	{
-		dest[len_dest + i] = src[i];
+		dest[ldest + i] = src[i];
 		i++;
 	}
-	dest[len_dest + i] = '\0';
-	len_dest = ft_strlen(dest);
-	return (len_dest);
+	dest[ldest + i] = '\0';
+	ldest = ft_strlen(dest);
+	return (ldest);
 }
 /*
 #include <stdio.h>
-#include <bsd/string.h>
 int main()
 {
 	char c1[255] = "Bonjour";
 	char c2[] = " je suis un test";
-	int r = ft_strlcat(c1, c2, 2);
+	int r = ft_strlcat(c1, c2, 18);
 	printf("%s, taille : %d", c1, r);
 }*/

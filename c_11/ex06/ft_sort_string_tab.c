b171309db77621cdd8f58aedb60776c6b6035c56
Mika Vogel <mivogel@student.fr>
2024-08-07 14:40:26 +0200

    c_11

---
+++ c_11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 13:55:40 by mivogel           #+#    #+#             */
+/*   Updated: 2024/08/07 14:27:30 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int		i;
+	int		j;
+	int		size;
+	char	*tmp;
+
+	i = 0;
+	size = ft_strlen(tab);
+	while (i < size)
+	{
+		j = 1;
+		while (j < size - 1)
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+				j = 0;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+/*
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	ft_sort_string_tab(argv);
+	i = 0;
+	while (++i < argc)
+		printf("%s\n", argv[i]);
+}*/

ca49cab1539f63be8b1120560a11efa3f5ecaf67
Mika Vogel <mivogel@student.fr>
2024-08-10 09:21:56 +0200

    c12

---
+++ c_12/ex01/ft_list_push_front.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_front.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/07 19:20:52 by mivogel           #+#    #+#             */
+/*   Updated: 2024/08/10 09:17:59 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list	*elem;
+
+	elem = malloc(sizeof(t_list));
+	if (elem)
+	{
+		elem->data = data;
+		elem->next = NULL;
+	}
+	return (elem);
+}
+
+void	ft_list_push_front(t_list **begin_list, void *data)
+{
+	t_list	*elem;
+
+	elem = ft_create_elem(data);
+	elem->next = *begin_list;
+	*begin_list = elem;
+}

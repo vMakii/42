b81d18dbd0f8273e36910529c28d1c060f516e0b
Mika Vogel <mivogel@student.fr>
2024-08-21 14:31:01 +0200

    c12

---
+++ c_12/ex07/ft_list_at.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_at.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/21 13:53:25 by mivogel           #+#    #+#             */
+/*   Updated: 2024/08/21 13:58:43 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (begin_list)
+	{
+		if (i == nbr)
+			return (begin_list);
+		begin_list = begin_list->next;
+		i++;
+	}
+	return (0);
+}

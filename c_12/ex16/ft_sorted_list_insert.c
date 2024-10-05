0bf958dcbb3a67512f2766146d946e20058ac248
Mika Vogel <mivogel@student.fr>
2024-10-05 22:31:13 +0200

    c12

--- c_12/ex16/ft_sorted_list_insert.c
+++ c_12/ex16/ft_sorted_list_insert.c
@@ -6,7 +6,7 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:25:49 by mivogel           #+#    #+#             */
-/*   Updated: 2024/10/04 18:40:41 by mivogel          ###   ########.fr       */
+/*   Updated: 2024/10/05 22:30:11 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,20 @@ t_list	*ft_create_elem(void *data)
 	return (elem);
 }
 
-void	ft_sorted_list_insert(t_list **begin_list, void *data)
+void	ft_sorted_list_insert(t_list **begin_list, void *data, int (*cmp)())
 {
+	t_list	*e;
+
+	if (!*begin_list || cmp(data, (*begin_list)->data) <= 0)
+	{
+		e = ft_create_elem(data);
+		if (!(e))
+			return ;
+		e->next = *begin_list;
+		*begin_list = e;
+	}
+	else
+	{
+		ft_sorted_list_insert(&(*begin_list)->next, data, cmp);
+	}
 }

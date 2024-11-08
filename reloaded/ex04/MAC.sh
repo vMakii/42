0c22fd5ea3cf2559940cee7f3734f5dbf54de49f
Mika Vogel <mivogel@bess-f4r3s14.clusters.42paris.fr>
2024-11-08 13:54:15 +0100

    jalskidfjlikdas

--- reloaded/ex04/MAC.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ifconfig | grep -o -E '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}'
-#-o print seulement le match et -E pour utiliser une expression
-#([[:xdigit:]]{2}:){5}[[:xdigit:]]{2} 6 groupes de 2 chiffres hexa separer d'un :
-
-#ifconfig | grep ether | cut -b 8-31

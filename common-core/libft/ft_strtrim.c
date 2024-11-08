01a95af1e48c8422db3e7d84b010bf67a0e4d8b7
Mika Vogel <mivogel@bess-f4r3s14.clusters.42paris.fr>
2024-11-08 15:21:31 +0100

    strtrim

---
+++ common-core/libft/ft_strtrim.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 13:56:22 by mivogel           #+#    #+#             */
+/*   Updated: 2024/11/08 15:20:49 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*str;
+	size_t	start;
+	size_t	end;
+
+	if (!str[i])
+		return (ft_strdup(""));
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end > start && ft_strchr(set, s1[end]))
+		end--;
+	str = (char *)malloc(sizeof(*s1))
+}

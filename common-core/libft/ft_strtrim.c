427da432410b42370017dbc7a956983c81add657
Mika Vogel <mivogel@student.fr>
2024-11-08 23:28:11 +0100

    finito cette horreur

--- common-core/libft/ft_strtrim.c
+++ common-core/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:56:22 by mivogel           #+#    #+#             */
-/*   Updated: 2024/11/08 15:20:49 by mivogel          ###   ########.fr       */
+/*   Updated: 2024/11/08 23:24:58 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
+	size_t	i;
 	size_t	start;
 	size_t	end;
 
-	if (!str[i])
-		return (ft_strdup(""));
 	start = 0;
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	end = ft_strlen(s1) - 1;
-	while (end > start && ft_strchr(set, s1[end]))
+	if (!s1[start])
+		return (ft_strdup(""));
+	end = ft_strlen(s1);
+	while (end > start && ft_strchr(set, s1[end - 1]))
 		end--;
-	str = (char *)malloc(sizeof(*s1))
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = '\0';
+	return (str);
 }

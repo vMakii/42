a79c1087cbcd5604b83269b717d630a32a100d07
Mika Vogel <mivogel@bess-f4r5s10.clusters.42paris.fr>
2024-11-12 18:06:36 +0100

    bonus

--- common-core/libft/ft_lstclear.c
+++ common-core/libft/ft_lstclear.c
@@ -1,21 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 17:37:09 by mivogel           #+#    #+#             */
-/*   Updated: 2024/11/12 17:47:25 by mivogel          ###   ########.fr       */
+/*   Created: 2024/11/12 17:52:05 by mivogel           #+#    #+#             */
+/*   Updated: 2024/11/12 18:01:08 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*tmp;
+
 	if (!lst || !del)
 		return ;
-	(*del)(lst->content);
-	free(lst);
+	tmp = *lst;
+	while (tmp)
+	{
+		ft_lstdelone(tmp, &(*del));
+		tmp = tmp->next;
+	}
 }
+//
+// void	ft_lstdelone(t_list *lst, void (*del)(void *))
+// {
+// 	if (!lst || !del)
+// 		return ;
+// 	(*del)(lst->content);
+// 	free(lst);
+// }
--- common-core/libft/ft_lstiter.c
+++ common-core/libft/ft_lstiter.c
@@ -1,21 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/12 17:37:09 by mivogel           #+#    #+#             */
-/*   Updated: 2024/11/12 17:47:25 by mivogel          ###   ########.fr       */
+/*   Created: 2024/11/12 17:50:20 by mivogel           #+#    #+#             */
+/*   Updated: 2024/11/12 17:51:14 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	if (!lst || !del)
-		return ;
-	(*del)(lst->content);
-	free(lst);
 }

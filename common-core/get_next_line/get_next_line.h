457cd506b721196a3f623c66a478b046a98f8ed5
Mika Vogel <mivogel@bess-f4r5s9.clusters.42paris.fr>
2024-11-19 12:10:29 +0100

    getnextline

---
+++ common-core/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 12:05:08 by mivogel           #+#    #+#             */
+/*   Updated: 2024/11/19 12:05:10 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_GET_NEXT_LINE_H
+# define FT_GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif

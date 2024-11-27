dc0e4dcd659e7e1ac3dce40251dd6764b22308d3
Mika Vogel <mivogel@bess-f4r5s10.clusters.42paris.fr>
2024-11-27 10:06:21 +0100

    42

--- common-core/libft/ft_lstmap.c
+++ common-core/libft/ft_lstmap.c
@@ -6,52 +6,32 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 10:00:24 by mivogel           #+#    #+#             */
-/*   Updated: 2024/11/14 13:03:41 by mivogel          ###   ########.fr       */
+/*   Updated: 2024/11/27 10:06:10 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_create_node(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	void	*content;
-	t_list	*node;
-
-	content = f(lst->content);
-	if (!content)
-	{
-		del(content);
-		return (NULL);
-	}
-	node = ft_lstnew(content);
-	if (!node)
-	{
-		del(content);
-		return (NULL);
-	}
-	return (node);
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*head;
-	t_list	*tmp;
+	t_list	*new;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	head = ft_create_node(lst, f, del);
-	if (!head)
-		return (NULL);
-	tmp = head;
-	while (lst->next)
+	head = NULL;
+	while (lst)
 	{
-		tmp->next = ft_create_node(lst->next, f, del);
-		if (!tmp->next)
+		content = f(lst->content);
+		new = ft_lstnew(content);
+		if (!new)
 		{
+			del(content);
 			ft_lstclear(&head, del);
 			return (NULL);
 		}
-		tmp = tmp->next;
+		ft_lstadd_back(&head, new);
 		lst = lst->next;
 	}
 	return (head);

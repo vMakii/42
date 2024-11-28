975cff51f6220c471f98461bd7f1c919823b974e
Mika Vogel <mivogel@student.42.fr>
2024-11-28 16:01:26 +0100

    16:00 2024-11-28

--- common-core/push_swap/srcs/swap.c
+++ common-core/push_swap/srcs/swap.c
@@ -6,24 +6,24 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 14:40:00 by mivogel           #+#    #+#             */
-/*   Updated: 2024/11/28 15:05:06 by mivogel          ###   ########.fr       */
+/*   Updated: 2024/11/28 15:17:51 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static void	swap(t_stack *stack)
+static void	swap(t_stack **stack)
 {
 	t_stack	*first;
 	t_stack	*second;
 
-	if (!stack || !stack->next)
+	if (!(*stack) || !(*stack)->next)
 		return ;
-	first = stack;
+	first = *stack;
 	second = first->next;
 	first->next = second->next;
 	second->next = first;
-	stack = second;
+	*stack = second;
 }
 
 // void	sa(t_stack **stack)

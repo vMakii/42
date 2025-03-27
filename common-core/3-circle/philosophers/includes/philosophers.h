3ca8af7e2295f74e52fb45fe4e8826e8e74571a2
Mika Vogel <mivogel@student.fr>
2025-03-27 21:24:21 +0100

    philo

--- common-core/3-circle/philosophers/includes/philosophers.h
+++ common-core/3-circle/philosophers/includes/philosophers.h
@@ -6,26 +6,36 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:47:35 by mivogel           #+#    #+#             */
-/*   Updated: 2025/03/27 18:58:55 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/03/27 20:44:49 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 # include <pthread.h>
+# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/time.h>
 # include <unistd.h>
 
+typedef struct s_philo
+{
+	int				id;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}					t_philo;
+
 typedef struct s_data
 {
-	int	num_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_meals;
-}		t_data;
+	int				num_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_meals;
+	t_philo			*philos;
+}					t_data;
 
 #endif
--- common-core/3-circle/philosophers/srcs/main.c
+++ common-core/3-circle/philosophers/srcs/main.c
@@ -6,14 +6,39 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 18:39:10 by mivogel           #+#    #+#             */
-/*   Updated: 2025/03/27 19:21:36 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/03/27 21:23:12 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	check_args(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (atoi(av[1]) > 200)
+		return (0);
+	while (av[++i])
+	{
+		j = -1;
+		while (av[i][++j])
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (0);
+		if (atoi(av[i]) <= 0)
+			return (0);
+	}
+	return (1);
+}
+
 void	ft_philos_init(t_data *data, int ac, char **av)
 {
+	if (!check_args(av))
+	{
+		printf("Error: Invalid arguments\n");
+		return ;
+	}
 	data->num_philo = atoi(av[1]);
 	data->time_to_die = atoi(av[2]);
 	data->time_to_eat = atoi(av[3]);

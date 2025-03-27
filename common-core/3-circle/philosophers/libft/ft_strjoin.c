968ac89a44937d9bcc47f2a4f90bd0af5bccd327
Mika Vogel <mivogel@student.42.fr>
2025-03-27 14:59:27 +0100

    3-circle

---
+++ common-core/3-circle/philosophers/libft/ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 13:36:05 by mivogel           #+#    #+#             */
+/*   Updated: 2024/11/12 13:53:52 by mivogel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*dst;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	dst = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!dst)
+		return (NULL);
+	ft_strlcpy(dst, s1, len1 + 1);
+	ft_strlcat(dst, s2, len1 + len2 + 1);
+	return (dst);
+}

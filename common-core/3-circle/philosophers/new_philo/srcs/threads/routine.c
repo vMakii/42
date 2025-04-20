c82abd0edcee45fd2760b8500ee3a4f7e84e5a1a
Mika Vogel <mivogel@student.fr>
2025-04-20 14:57:09 +0200

    philo

--- common-core/3-circle/philosophers/new_philo/srcs/threads/routine.c
+++ common-core/3-circle/philosophers/new_philo/srcs/threads/routine.c
@@ -6,7 +6,7 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 10:34:31 by mivogel           #+#    #+#             */
-/*   Updated: 2025/04/18 00:11:25 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/04/19 18:38:46 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,9 @@ void	*ft_routine(void *ptr)
 
 void	ft_eat(t_philo *philo)
 {
+	pthread_mutex_t	*first;
+	pthread_mutex_t	*second;
+
 	if (philo->data->num_philo == 1)
 	{
 		pthread_mutex_lock(philo->left_fork);
@@ -51,29 +54,63 @@ void	ft_eat(t_philo *philo)
 		ft_usleep(philo->data->time_to_die + 1);
 		pthread_mutex_unlock(philo->left_fork);
 	}
-	else if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(philo->left_fork);
-		ft_print("has taken a fork", philo->id, philo, KCYN);
-		pthread_mutex_lock(philo->right_fork);
-		ft_print("has taken a fork", philo->id, philo, KCYN);
-	}
-	else
-	{
-		pthread_mutex_lock(philo->right_fork);
-		ft_print("has taken a fork", philo->id, philo, KCYN);
-		pthread_mutex_lock(philo->left_fork);
-		ft_print("has taken a fork", philo->id, philo, KCYN);
-	}
+	ft_get_forks_in_order(philo->left_fork, philo->right_fork, &first, &second);
+	pthread_mutex_lock(first);
+	ft_print("has taken a fork", philo->id, philo, KCYN);
+	pthread_mutex_lock(second);
+	ft_print("has taken a fork", philo->id, philo, KCYN);
 	pthread_mutex_lock(&philo->meal_lock);
 	ft_print("is eating", philo->id, philo, KGRN);
 	philo->last_meal = ft_get_time();
 	philo->num_meals++;
 	pthread_mutex_unlock(&philo->meal_lock);
 	ft_usleep(philo->data->time_to_eat);
-	pthread_mutex_unlock(philo->left_fork);
-	pthread_mutex_unlock(philo->right_fork);
+	pthread_mutex_unlock(second);
+	pthread_mutex_unlock(first);
 }
+
+// void	ft_eat(t_philo *philo)
+// {
+// 	pthread_mutex_t	*first;
+// 	pthread_mutex_t	*second;
+
+// 	if (philo->data->num_philo == 1)
+// 	{
+// 		pthread_mutex_lock(philo->left_fork);
+// 		ft_print("has taken a fork", philo->id, philo, KCYN);
+// 		ft_usleep(philo->data->time_to_die + 1);
+// 		pthread_mutex_unlock(philo->left_fork);
+// 	}
+// else if (philo->id % 2 == 0)
+// {
+// 	pthread_mutex_lock(philo->left_fork);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// 	pthread_mutex_lock(philo->right_fork);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// }
+// else
+// {
+// 	pthread_mutex_lock(philo->right_fork);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// 	pthread_mutex_lock(philo->left_fork);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// }
+// 	ft_get_forks_in_order(philo->left_fork, philo->right_fork, &first, &second);
+// 	pthread_mutex_lock(first);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// 	pthread_mutex_lock(second);
+// 	ft_print("has taken a fork", philo->id, philo, KCYN);
+// 	pthread_mutex_lock(&philo->meal_lock);
+// 	ft_print("is eating", philo->id, philo, KGRN);
+// 	philo->last_meal = ft_get_time();
+// 	philo->num_meals++;
+// 	pthread_mutex_unlock(&philo->meal_lock);
+// 	ft_usleep(philo->data->time_to_eat);
+// 	pthread_mutex_unlock(second);
+// 	pthread_mutex_unlock(first);
+// pthread_mutex_unlock(philo->left_fork);
+// pthread_mutex_unlock(philo->right_fork);
+// }
 
 void	ft_sleep(t_philo *philo)
 {

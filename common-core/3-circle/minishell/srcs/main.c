46eb342642de3a70ce0e2549707d12a191ad99aa
Mika Vogel <mivogel@student.fr>
2025-04-24 21:51:09 +0200

    minishell

--- common-core/3-circle/minishell/srcs/main.c
+++ common-core/3-circle/minishell/srcs/main.c
@@ -6,18 +6,23 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:00:59 by mivogel           #+#    #+#             */
-/*   Updated: 2025/04/24 16:50:16 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/04/24 18:39:52 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_init_data(t_data *data, int ac, char **av)
+void	ft_init_data(t_data *data, int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
 	data->prompt = NULL;
-	data->env = NULL;
+	data->env = ft_env(data, env);
+	if (data->env == NULL)
+	{
+		perror("ft_env");
+		exit(EXIT_FAILURE);
+	}
 	data->exit_status = 0;
 }
 
@@ -33,7 +38,7 @@ int	main(int ac, char **av, char **env)
 	t_data data;
 
 	ft_memset(&data, 0, sizeof(t_data));
-	ft_init_data(&data, ac, av);
+	ft_init_data(&data, ac, av, env);
 	ft_free(&data);
 	return (0);
 }

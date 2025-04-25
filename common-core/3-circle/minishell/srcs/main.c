417491cd958c008cf135815c2334443c4047e1cd
Mika Vogel <mivogel@student.42.fr>
2025-04-25 12:11:03 +0200

    update

--- common-core/3-circle/minishell/srcs/main.c
+++ common-core/3-circle/minishell/srcs/main.c
@@ -6,18 +6,32 @@
 /*   By: mivogel <mivogel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:00:59 by mivogel           #+#    #+#             */
-/*   Updated: 2025/04/24 18:39:52 by mivogel          ###   ########.fr       */
+/*   Updated: 2025/04/25 10:13:07 by mivogel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+pid_t		g_pid;
+
+static char	**ft_env(char **envp)
+{
+	int		len;
+	char	**env;
+
+	len = ft_strlen(envp);
+	env = malloc(sizeof(char *) * len);
+	if (!env)
+		return (NULL);
+	return (env);
+}
 
 void	ft_init_data(t_data *data, int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
 	data->prompt = NULL;
-	data->env = ft_env(data, env);
+	data->env = ft_env(env);
 	if (data->env == NULL)
 	{
 		perror("ft_env");

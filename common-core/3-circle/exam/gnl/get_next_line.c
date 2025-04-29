1fc74308b141e52a1446d83c6386134b16cda9a7
Mika Vogel <mivogel@student.42.fr>
2025-04-29 11:21:39 +0200

    update

---
+++ common-core/3-circle/exam/gnl/get_next_line.c
@@ -0,0 +1,51 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+// stdio is not needed, just let him for test with main
+#include <stdio.h>
+
+char	*get_next_line(int fd)
+{
+	char	*ret;
+	char	c;
+	int		i;
+	int		cpt;
+
+	ret = malloc(9999);
+	i = 0;
+	cpt = 0;
+	if (fd < 0)
+		return (NULL);
+	while ((cpt = read(fd, &c, 1)) > 0)
+	{
+		ret[i] = c;
+		i++;
+		if (c == '\n')
+			break ;
+	}
+	if (i == 0 || cpt < 0)
+	{
+		free(ret);
+		return (NULL);
+	}
+	ret[i] = '\0';
+	return (ret);
+}
+
+// Main is here only for testing, do not let him in exam
+int	main(int argc, char **argv)
+{
+	int fd = 0;
+	char *line;
+
+	if (argc > 1)
+		fd = open(argv[1], O_RDONLY);
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		printf("line | %s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	return (0);
+}
---
+++ common-core/3-circle/exam/gnl/get_next_line.h
@@ -0,0 +1,14 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <unistd.h>
+
+#endif

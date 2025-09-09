718ddd7f278425c8165e4d29b11eb8fa41778c3f
Mika Vogel <mivogel@student.42.fr>
2025-09-09 14:21:35 +0200

    Add min/max static member functions to Fixed class

--- common-core/4-circle/modules_cpp/cpp-02/ex02/srcs/Fixed.cpp
+++ common-core/4-circle/modules_cpp/cpp-02/ex02/srcs/Fixed.cpp
@@ -143,6 +143,26 @@ void Fixed::setRawBits(int const raw)
 }
 
 // Member functions
+Fixed& Fixed::min(Fixed& a, Fixed& b)
+{
+    return (a < b) ? a : b;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
+{
+    return (a < b) ? a : b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b)
+{
+    return (a > b) ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b)
+{
+    return (a > b) ? a : b;
+}
+
 float Fixed::toFloat() const
 {
     return static_cast<float>(_rawBits) / (1 << _fractionalBits);

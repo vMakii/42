da52548f921f58e377fba6ad0fdc9b21b63d4437
Mika Vogel <mivogel@student.42.fr>
2025-10-29 21:18:13 +0100

    update

---
+++ bootcamp_python/module02/ex00/ft_reduce.py
@@ -0,0 +1,23 @@
+def ft_reduce(function_to_apply, iterable):
+    """Apply function of two arguments cumulatively.
+    Args:
+        function_to_apply: a function taking an iterable.
+        iterable: an iterable object (list, tuple, iterator).
+    Return:
+        A value, of same type of elements in the iterable parameter.
+        None if the iterable can not be used by the function.
+    """
+    iterator = iter(iterable)
+    try:
+        result = next(iterator)
+    except StopIteration:
+        return None
+    for item in iterator:
+        result = function_to_apply(result, item)
+    return result
+
+# Example 3:
+lst = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
+print(ft_reduce(lambda u, v: u + v, lst))
+# Output:
+# "Hello world"
---
+++ bootcamp_python/module02/ex01/main.py
@@ -0,0 +1,72 @@
+def what_are_the_vars(*args, **kwargs):
+    """
+    Returns an instance of ObjectC
+    """
+    obj = ObjectC()
+    
+    # Set attributes from *args as var_0, var_1, var_2, etc.
+    for i, arg in enumerate(args):
+        setattr(obj, f"var_{i}", arg)
+    
+    # Set attributes from **kwargs
+    for key, value in kwargs.items():
+        if getattr(obj, key, None) is not None:
+            return None
+        setattr(obj, key, value)
+    
+    return obj
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars(None, [])
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, "Yes", a=10, var_2="world")
+    doom_printer(obj)
+
+# $> python main.py
+# var_0: 7
+# end
+# var_0: None
+# var_1: []
+# end
+# var_0: ft_lol
+# var_1: Hi
+# end
+# end
+# a: 10
+# hello: world
+# var_0: 12
+# var_1: Yes
+# var_2: [0, 0, 0]
+# end
+# ERROR
+# end
+# a: 10
+# var_0: 42
+# var_1: Yes
+# var_2: world
+# end
